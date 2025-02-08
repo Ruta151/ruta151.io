81f22877a61278772153aaeaf8f1dbe5adfc66fa
Ruta151 <diegodelfa77@gmail.com>
2025-02-08 16:41:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 const productsList = [
     { name: "Pokemon TCG: Scarlet & Violet 9 – Journey Together - Elite Trainer Box", description: "Primera prueba", languaje: "Español", price: "69.95 €", image: "images/prismatic1.jpeg"},
     { name: "Pokemon TCG: Scarlet & Violet 9 – Journey Together - Booster CDU (36)", description: "Segunda prueba", languaje: "Inglés", price: "69.95 €", image: "images/prismatic1.jpeg"},
-    { name: "Riñon en perfectas condiciones", description: "Tercera", languaje: "Alemán", price: "215.95 €", image: "images/prismatic1.jpeg"},
-    { name: "Super premium collection", description: "Ya no más Ya no más", languaje: "Japonés", price: "139,95 €", image: "images/prismaticultra.jpg"},
-    { name: "Otro más de prueba 5", description: "Ya no más", languaje: "Japonés", price: "69.95 €", image: "images/prismatic1.jpeg"},
-    { name: "Otro más de prueba 6", description: "Ya no más", languaje: "Japonés", price: "999.95 €", image: "images/prismatic1.jpeg"},
-    { name: "Otro más de prueba 7", description: "Ya no más", languaje: "Japonés", price: "69.95 €", image: "images/prismatic1.jpeg"},
-    { name: "Otro más de prueba 8", description: "Ya no más", languaje: "Japonés", price: "69.95 €", image: "images/prismatic1.jpeg"},
-    { name: "Otro más de prueba 9", description: "Ya no más", languaje: "Japonés", price: "69.95 €", image: "images/prismatic1.jpeg"},
-];
+{ name: "Evoluciones Prismáticas Colección Super Premium", description: "Caja Evoluciones Prismáticas Super Premium Español (RESERVA)", languaje: "Español", price: "139.95 €", image: "images/prismaticultra.jpg"}];
 
 const productListElement = document.getElementById("product-list");
 
